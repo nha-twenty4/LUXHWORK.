74ca0ac80ac93639587ec11da7e40e2f0d03fe24
nha-twenty4 <1.25654963e+08+nha-twenty4@users.noreply.github.com>
2026-09-19 04:33:36 +0000

    Fix consultation form API on Vercel

---
+++ api/trpc/[trpc].ts
@@ -0,0 +1,17 @@
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { appRouter } from "../../server/routers";
+import { createContext } from "../../server/_core/context";
+
+const middleware = createExpressMiddleware({
+  router: appRouter,
+  createContext,
+});
+
+export default function handler(req: any, res: any) {
+  return middleware(req, res, () => {
+    if (!res.headersSent) {
+      res.statusCode = 404;
+      res.end();
+    }
+  });
+}
